3be9946dea5c81c617c3e8218487957c1a2aa36b
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-07-26 23:46:44 +0800

    IA-16 __far: suppress iv optimization for far pointers
    This fixes a rather bad code correctness issue when a far
    pointer is involved in a loop.  The test cases gcc.target/
    ia16/far-addr-{7, 8}.c should now pass.

--- gcc/testsuite/gcc.target/ia16/far-addr-8.c
+++ gcc/testsuite/gcc.target/ia16/far-addr-8.c
@@ -1,13 +1,12 @@
 /* { dg-do run } */
-/* { dg-xfail-if "" *-*-* } */
-/* { dg-xfail-run-if "" *-*-* } */
 /* { dg-options "-O3 -fno-inline --save-temps" } */
 
-/* FAIL: Far pointers to non-volatile things.  For the same read-only memory
-   area, using non-volatile pointers should give the same results as using
-   volatile pointers.
+/* Far pointers to non-volatile things.  For the same read-only memory area,
+   using pointers to non-`volatile' should give the same results as using
+   pointers to `volatile'.
 
-   Currently this crashes the compiler.  */
+   We also include further tests where the _pointers_ themselves are made
+   volatile (not necessarily the things pointed to).  */
 
 int printf (const char *, ...);
 
@@ -17,7 +16,7 @@ hash1 (void)
   unsigned __far *p = (unsigned __far *) 0xf0000000ul;
   unsigned i;
   unsigned long h = 0;
-  for (i = 0; i < 2000; ++i)
+  for (i = 0; i < 0x8000u; ++i)
     h = 5 * h ^ *p++;
   return h;
 }
@@ -28,7 +27,30 @@ hash2 (void)
   volatile unsigned __far *p = (volatile unsigned __far *) 0xf0000000ul;
   unsigned i;
   unsigned long h = 0;
-  for (i = 0; i < 2000; ++i)
+  for (i = 0; i < 0x8000u; ++i)
+    h = 5 * h ^ *p++;
+  return h;
+}
+
+unsigned long
+hash3 (void)
+{
+  unsigned __far * volatile p = (unsigned __far *) 0xf0000000ul;
+  unsigned i;
+  unsigned long h = 0;
+  for (i = 0; i < 0x8000u; ++i)
+    h = 5 * h ^ *p++;
+  return h;
+}
+
+unsigned long
+hash4 (void)
+{
+  volatile unsigned __far * volatile p =
+    (volatile unsigned __far *) 0xf0000000ul;
+  unsigned i;
+  unsigned long h = 0;
+  for (i = 0; i < 0x8000u; ++i)
     h = 5 * h ^ *p++;
   return h;
 }
@@ -36,7 +58,7 @@ hash2 (void)
 int
 main (void)
 {
-  unsigned long h1 = hash1 (), h2 = hash2 ();
-  printf ("%#lx %#lx\n", h1, h2);
-  return h1 == h2 ? 0 : 1;
+  unsigned long h1 = hash1 (), h2 = hash2 (), h3 = hash3 (), h4 = hash4 ();
+  printf ("%#lx %#lx %#lx %#lx\n", h1, h2, h3, h4);
+  return h1 == h2 && h1 == h3 && h1 == h4 ? 0 : 1;
 }
