5e98eaacc708e93f80ef1f380a640d8523e4ff3c
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-07-27 22:58:43 +0800

    IA-16 __far: more rigorous far-addr-7.c test case (which earlier got lost in a "git rebase")

--- gcc/testsuite/gcc.target/ia16/far-addr-7.c
+++ gcc/testsuite/gcc.target/ia16/far-addr-7.c
@@ -1,17 +1,32 @@
 /* { dg-do run } */
 /* { dg-options "-O3 -fno-inline --save-temps" } */
 
-/* Using a far pointer to loop through an area of (emulated) BIOS ROM.  */
+/* Using a far pointer to `volatile' loop through an area of (emulated) BIOS
+   ROM.  The optimized routine should give the exact same results as the
+   unoptimized routine.  */
 
 int printf (const char *, ...);
 
+__attribute__ ((optimize ("O3")))
 unsigned long
-hash (void)
+hash1 (void)
 {
-  volatile unsigned __far *p = (volatile unsigned __far *) 0xf0000000ul;
+  volatile unsigned __far *p = (volatile unsigned __far *) 0xfff00000ul;
   unsigned i;
   unsigned long h = 0;
-  for (i = 0; i < 2000; ++i)
+  for (i = 0; i < 0x80u; ++i)
+    h = 5 * h ^ *p++;
+  return h;
+}
+
+__attribute__ ((optimize ("O0")))
+unsigned long
+hash2 (void)
+{
+  volatile unsigned __far *p = (volatile unsigned __far *) 0xfff00000ul;
+  unsigned i;
+  unsigned long h = 0;
+  for (i = 0; i < 0x80u; ++i)
     h = 5 * h ^ *p++;
   return h;
 }
@@ -19,6 +34,7 @@ hash (void)
 int
 main (void)
 {
-  printf ("%#lx\n", hash ());
-  return 0;
+  unsigned long h1 = hash1 (), h2 = hash2 ();
+  printf ("%#lx %#lx\n", h1, h2);
+  return h1 == h2 ? 0 : 1;
 }
