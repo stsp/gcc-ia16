1941419f5cc0b784075ce15bed72af6ffb6762f9
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2018-02-02 11:34:46 +0800

    IA-16: add small test case for `xlat %ss:(%bx)'

---
+++ gcc/testsuite/gcc.target/ia16/xlat-1.c
@@ -0,0 +1,18 @@
+/* { dg-do run } */
+/* { dg-options "-O3 --save-temps" } */
+
+/* Test that `xlat' instructions are prefixed with the proper segment override
+   if %ds != %ss and if we are optimizing.  */
+
+void abort (void);
+
+unsigned char
+foo (unsigned char x)
+{
+  static unsigned char map[] = "0123456789abcdef";
+
+  __asm volatile ("" : : "a" (x), "b" (map), "Rds" (0));
+  return map[x];
+}
+
+/* { dg-final { scan-assembler "xlat\[ \\t\]%ss:" } } */
