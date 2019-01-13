b8a9ef8fc3ffef9f96cd09b703a8b38359ccd228
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2019-01-13 23:19:27 +0800

    IA-16: fix: omit linker script upon -r (relocatable link)

--- gcc/config/ia16/elf.h
+++ gcc/config/ia16/elf.h
@@ -27,24 +27,32 @@
   "%{!mno-segment-relocation-stuff:" \
     "%{mcmodel=small|mcmodel=medium:-msegment-relocation-stuff}}"
 
-/* Arrange for the linker script specification (%T...) to appear in LIB_SPEC
-   if we are linking in default libraries, but in LINK_SPEC if we are not
-   (i.e. -nostdlib or -nodefaultlibs).
+/* For -nostdlib, -nodefaultlibs, and -nostartfiles:  arrange for the linker
+   script specification (%T...) to appear in LIB_SPEC if we are linking in
+   default libraries, but in LINK_SPEC if we are not (i.e. -nostdlib or
+   -nodefaultlibs).
 
    If default libraries are used, then we want them to appear at the end of
    the ld command line, so LIB_SPEC is the right place to use.
 
    If default libraries are not used, then GCC will not use LIB_SPEC at all,
-   so we need to rely on LINK_SPEC to pass the linker script to ld.  -- tkchia
- */
+   so we need to rely on LINK_SPEC to pass the linker script to ld.
+
+   As for the (undocumented) -r option:  I currently just arrange not to
+   pass any linker script at all if -r appears.  (FIXME: This only really
+   works if something like -nostdlib is also specified.  One is out of luck
+   if they would also like to link in startup files or default libraries
+   during -r.)  -- tkchia  */
 #define LINK_SPEC	\
   "%{!T*:"		\
-    "%{nostdlib|nodefaultlibs:" \
-      "%{mcmodel=small:"	\
-	"%{melks:%Telks-separate.ld;" \
-	  "nostdlib|nostartfiles:%Tdos-ms.ld;:%Tdos-mss.ld};" \
-	"melks:%Telks-combined.ld;" \
-	"nostdlib|nostartfiles:%Tdos-mt.ld;:%Tdos-mts.ld" \
+    "%{!r:"		\
+      "%{nostdlib|nodefaultlibs:" \
+	"%{mcmodel=small:" \
+	  "%{melks:%Telks-separate.ld;" \
+	    "nostdlib|nostartfiles:%Tdos-ms.ld;:%Tdos-mss.ld};" \
+	  "melks:%Telks-combined.ld;" \
+	  "nostdlib|nostartfiles:%Tdos-mt.ld;:%Tdos-mts.ld" \
+	"}"		\
       "}"		\
     "}"			\
   "}"
@@ -57,13 +65,15 @@
 
 #define LIB_SPEC	\
   "%{!T*:"		\
-    "%{!nostdlib:"	\
-      "%{!nodefaultlibs:" \
-	"%{mcmodel=small:" \
-	  "%{melks:%Telks-separate.ld;" \
-	    "nostartfiles:%Tdos-msl.ld;:%Tdos-mssl.ld};" \
-	  "melks:%Telks-combined.ld;" \
-	  "nostartfiles:%Tdos-mtl.ld;:%Tdos-mtsl.ld" \
+    "%{!r:"		\
+      "%{!nostdlib:"	\
+	"%{!nodefaultlibs:" \
+	  "%{mcmodel=small:" \
+	    "%{melks:%Telks-separate.ld;" \
+	      "nostartfiles:%Tdos-msl.ld;:%Tdos-mssl.ld};" \
+	    "melks:%Telks-combined.ld;" \
+	    "nostartfiles:%Tdos-mtl.ld;:%Tdos-mtsl.ld" \
+	  "}"		\
 	"}"		\
       "}"		\
     "}"			\
